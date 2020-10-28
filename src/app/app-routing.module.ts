da8b7f5034d095c923fd9071d0553a89302fefed
soutionscience <marshnjagi@yahoo.com>
2020-10-28 09:12:01 +0300

    started off well

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./PAGES/home/home.module').then( m => m.HomePageModule)
   }
+
 ];
 
 @NgModule({
